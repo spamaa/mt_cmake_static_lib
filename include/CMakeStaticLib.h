308204e8d6b2a35487d25fbaede29e566c653b87
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-06 09:46:50 +0200

    compileable cmake-static-lib as a seperate project

---
+++ include/CMakeStaticLib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// version 1.1.0
+
+#if !defined(CMAKESTATICLIB_EXPORT)
+#define CMAKESTATICLIB_EXPORT
+#endif
+
+namespace CMakeStaticLib
+{
+    void About();
+}
---
+++ src/CMakeStaticLib.cpp
@@ -0,0 +1,14 @@
+#include <CMakeStaticLib.h>
+#include <iostream>
+
+namespace CMakeStaticLib
+{
+	void About()
+	{
+		::std::cout << "Hello from CMakeStaticLib" << ::std::endl;
+		::std::cout << __FUNCTION__ << ":" << __FILE__ << ":" << __LINE__ << std::endl;
+		::std::cout << "=== 2.0.0 ===" << ::std::endl;
+	}
+
+}
+
